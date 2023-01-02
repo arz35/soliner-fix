8a94d8bb439ad3140a2b8bd8c45b78e73eaa0a4d
arz35 <113775553+arz35@users.noreply.github.com>
2023-01-03 02:05:42 +0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,6 +6,19 @@ import Services from './pages/Services'
 import About from './pages/About'
 import Contact from './pages/Contact'
 import Career from './pages/Career'
+import Contracting from './pages/sector/Contracting';
+import DisciplineEnginering from './pages/sector/DisciplineEnginering';
+import Finance from './pages/sector/Finance';
+import InformationTechnology from './pages/sector/InformationTechnology';
+import Marketing from './pages/sector/Marketing';
+import ProductionEngineering from './pages/sector/ProductionEngineering';
+import AssistantMarketing from './pages/jobDesk/AssistantMarketing';
+import AssociateAdmin from './pages/jobDesk/AssociateAdmin';
+import ConstructionOEM from './pages/jobDesk/ConstructionOEM';
+import CreditAssesment from './pages/jobDesk/CreditAssesment';
+import LegalCounsel from './pages/jobDesk/LegalCounsel';
+import ProccesEngineer from './pages/jobDesk/ProccesEngineer';
+import QualityAssurance from './pages/jobDesk/QualityAssurance';
 
 const App = () => {
   return (
@@ -17,6 +30,19 @@ const App = () => {
             <Route path="/about" element={<About/>} />
             <Route path="/contact" element={<Contact/>}/>
             <Route path="/career" element={<Career/>}/>
+            <Route path="/sector-1" element={<Contracting />}/>
+            <Route path="/sector-2" element={<DisciplineEnginering />}/>
+            <Route path="/sector-3" element={<Finance />}/>
+            <Route path="/sector-4" element={<InformationTechnology />}/>
+            <Route path="/sector-5" element={<Marketing />}/>
+            <Route path="/sector-6" element={<ProductionEngineering />}/>
+            <Route path="/job-1" element={<AssociateAdmin />}/>
+            <Route path="/job-2" element={<CreditAssesment />}/>
+            <Route path="/job-3" element={<LegalCounsel />}/>
+            <Route path="/job-4" element={<ProccesEngineer />}/>
+            <Route path="/job-5" element={<ConstructionOEM />}/>
+            <Route path="/job-6" element={<QualityAssurance />}/>
+            <Route path="/job-7" element={<AssistantMarketing />}/>
           </Routes>
         </BrowserRouter>
       </div>
